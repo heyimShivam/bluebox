4a65c9c29da55ee30bf1c2d391777d8d1dfc9780
Ubuntu <ubuntu@ip-172-31-31-31.us-east-2.compute.internal>
2021-06-08 15:30:35 +0000

    allow host

--- bluebox/settings.py
+++ bluebox/settings.py
@@ -155,7 +155,7 @@ LOGGING = {
         'file': {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
-            'filename': "/home/kishan/projects/fivver/bluebox/backend/bluebox/custom_logs/debug.log",
+            'filename': "/home/project/bluebox/custom_logs/debug.log",
         },
     },
     'loggers': {
